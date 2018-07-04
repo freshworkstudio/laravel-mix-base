082e0b27732e97922f3d23a31e6fc17a7d745ae8
Gonzalo De Spirito <gonzunigad@gmail.com>
2018-07-03 20:46:13 -0400

    Fix path problem on sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ let mix = require('laravel-mix');
 
 
 mix
+	.setPublicPath('public/')
+	.setResourceRoot('../')
 	//.js('src/js/app.js', 'public/js') // Toma lo que está en src/js/app.js, lo compila y el resultado quedará en la carpeta public/js/
 	.sass('src/scss/app.scss', 'public/css'); // Toma el archivo SASSS (.scss) que está en src/scss/app.scss, lo compila y lo deja en public/css/app.css
 
